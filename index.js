39d57bdc879874a13cffb2a1d89f2528aa732d2a
Brian Clayton <finalb@projectfinalboss.com>
2020-07-09 18:04:41 -0400

    update server code

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
+let user = [];
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) =>{
+   const userInfo =  req.body;
+
+    userInfo.id = shortid.generate();
+    user.push(userInfo);
+
+    res.status(201).json(userInfo)
+
+})
+
+server.get('/api/users', (req, res) => {
+res.json(users);
+});
+
+server.get('/api/users/:id', (req, res) => {
+    res.json(users);
+    });
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log('listening on localhost:', PORT);
+});
